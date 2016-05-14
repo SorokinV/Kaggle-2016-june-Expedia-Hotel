acfa7a7f59ec76d319f73684eba9be7ab7f48e0b
boba2 <sorokinv@hotmail.com>
2016-05-14 21:22:42 +1000

    #6 Work on leak data

--- Python/PreProcess.py
+++ Python/PreProcess.py
@@ -1,6 +1,11 @@
+
 import math
 import os.path
 import datetime
+from heapq import nlargest
+
+import sys
+import gc
 
 import csv
 import pickle
@@ -33,17 +38,29 @@ class PreTrain (BigCSVFile) :
     inputFile = ""
     outBook0  = "../Data/trainBook0.csv"
     outBook1  = "../Data/trainBook1.csv"
+
+    outD1     = "../Data/DataLeak/leak1.csv"
+    
     numBook     = 18
     numDateTime = 0
     numDateBeg  = 11
     numDateEnd  = 12
+
+    # for leak data
+    numULC      =  5  # user_location_city
+    numODD      =  6  # orig_destination_distance
+    numSDI      = 16  # srch_destination_id
+    numHCluster = 23  # hotel_cluster
+    numHCountry = 21  # hotel_country 
+    numHMarket  = 22  # hotel_market
+    
     def __init__ (self,input) :
         self.inputFile = input;
         self.readHeader(input)
     def readSplitIsBooking(self) :
         with open(self.inputFile, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            i0, i1 = 0,0
+            i0, i1, ie = 0,0,0; le =[]
             for row in reader :
                  if reader.line_num==1:
                      wrt0 = open(self.outBook0,"w")
@@ -58,16 +75,31 @@ class PreTrain (BigCSVFile) :
                      out1.writerow(h01)
                      continue;
                  dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
-                 if (row[self.numDateBeg]=="") : dateBeg=datetime.datetime(2000,1,1)
-                 else : dateBeg  = datetime.datetime.strptime(row[self.numDateBeg],"%Y-%m-%d")
-                 if (row[self.numDateEnd]=="") : dateEnd=datetime.datetime(2000,1,1)
-                 else : dateEnd  = datetime.datetime.strptime(row[self.numDateEnd],"%Y-%m-%d")
+                 
+                 try :
+                     dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
+                 except :
+                     dateTime = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                 try :
+                     dateBeg  = datetime.datetime.strptime(row[self.numDateBeg],"%Y-%m-%d")
+                 except :
+                     dateBeg  = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                 try :
+                     dateEnd  = datetime.datetime.strptime(row[self.numDateEnd],"%Y-%m-%d")
+                 except :
+                     dateEnd  = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                     
                  if self.debug : print(dateTime,dateBeg,dateEnd)
+                 
                  dateTimeV= [dateTime.year,dateTime.month,dateTime.day,dateTime.weekday(),
                              dateTime.hour,dateTime.minute]
                  dateBegV = [dateBeg.year,dateBeg.month,dateBeg.day,dateBeg.weekday()]
                  dateEndV = [dateEnd.year,dateEnd.month,dateEnd.day,dateEnd.weekday()]
-                 datesNN  = [(dateEnd-dateBeg).days+1]
+                 
+                 datesNN  = (dateEnd-dateBeg).days+1
+                 if (not((0<=datesNN) and (datesNN<=200))) : datesNN=0
+                 datesNN=[datesNN]
+                 
                  rowX = dateTimeV+row[1:self.numDateBeg]+dateBegV+dateEndV+datesNN+row[(self.numDateEnd+1):]
                  if self.debug : print(dateTimeV,dateBegV,dateEndV,datesNN)
                  if row[self.numBook]=='0' :
@@ -81,6 +113,107 @@ class PreTrain (BigCSVFile) :
             wrt1.close();
             print("(0,1)=",i0,i1)
             return
+        
+    def getLeak(self) :
+
+        d1 = dict();
+        d2 = dict();
+        d3 = dict();
+        d4 = dict();
+        cluster0 = [0]*100
+        
+        with open(self.inputFile, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            i0, i1, ie = 0,0,0; le =[]
+            for row in reader :
+                 if reader.line_num==1:
+                     #wrt0 = open(self.outBook0,"w")
+                     #wrt1 = open(self.outBook1,"w")
+                     #out0 = csv.writer(wrt0,delimiter=',', quotechar='|')
+                     #out1 = csv.writer(wrt1,delimiter=',', quotechar='|')
+                     continue;
+                    
+                 dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
+                 
+                 try :
+                     dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
+                 except :
+                     dateTime = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                 try :
+                     dateBeg  = datetime.datetime.strptime(row[self.numDateBeg],"%Y-%m-%d")
+                 except :
+                     dateBeg  = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                 try :
+                     dateEnd  = datetime.datetime.strptime(row[self.numDateEnd],"%Y-%m-%d")
+                 except :
+                     dateEnd  = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                     
+                 if self.debug : print(dateTime,dateBeg,dateEnd)
+                 
+                 datesNN  = (dateEnd-dateBeg).days+1
+                 if (not((0<=datesNN) and (datesNN<=200))) : datesNN=0
+                 datesNN=[datesNN]
+
+                 isBooking = int(row[self.numBook])
+                 ULC       = row[self.numULC]
+                 ODD       = row[self.numODD]
+                 SDI       = row[self.numSDI]
+                 HMarket   = row[self.numHMarket]
+                 HCountry  = row[self.numHCountry]
+                 HCluster  = int(row[self.numHCluster])
+
+                 app1      = 3+17*isBooking
+                 app2      = 1+ 5*isBooking
+
+                 if ((ULC!="") and (ODD!="")) :
+                     key = (ULC,ODD)
+                     cluster = d1.get(key,cluster0)
+                     cluster[HCluster] += 1
+                     d1[key]=cluster
+
+                 if ((SDI!="") and (HCountry!="") and (HMarket!="") and (dateTime.year==2014)) :
+                     key = (SDI,HCountry,HMarket)
+                     cluster = d2.get(key,cluster0)
+                     cluster[HCluster] += app1
+                     d2[key]=cluster
+
+                 if ((SDI!="")) :
+                     key = (SDI)
+                     cluster = d3.get(key,cluster0)
+                     cluster[HCluster] += app1
+                     d3[key]=cluster
+
+                 if ((HCountry!="")) :
+                     key = (HCountry)
+                     cluster = d4.get(key,cluster0)
+                     cluster[HCluster] += app2
+                     d4[key]=cluster
+
+                 if self.debug : print(dateTimeV,dateBegV,dateEndV,datesNN)
+                 if reader.line_num>30000 : break
+                 if reader.line_num%10000==0 : print(reader.line_num)
+            csvfile.close();
+            #wrt0.close();
+            #wrt1.close();
+            print(len(d1),len(d2),len(d3),len(d4))
+            print(list(d1.keys())[0:1])
+            print(list(d1.values())[0:1])
+            print("(0,1)=",i0,i1)
+
+            self.writeDD(self.outD1,d1);
+            
+            return
+        
+    def writeDD (self,outFile,dd) :
+        wrt0 = open(outFile,"w")
+        out0 = csv.writer(wrt0,delimiter=',', quotechar='|')
+        h01  = ["user_location_city","orig_destination_distance",'p1','p2','p3','p4',"p5"]
+        out0.writerow(h01)
+        for i in dd.keys() :
+            value  = dd[i]; value.sort(reverse=True)
+            row = [i[0],i[1],value[0],value[1],value[2],value[3],value[4]]
+            out0.writerow(row)
+        wrt0.close();
         
 class PreTest (BigCSVFile) :
 
@@ -123,22 +256,25 @@ class PreTest (BigCSVFile) :
                  try :
                      dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
                  except :
-                     dateTime = datetime.datetime(2000,1,1); ie+=1; le.append(reader.line_num)
+                     dateTime = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
                  try :
                      dateBeg  = datetime.datetime.strptime(row[self.numDateBeg],"%Y-%m-%d")
                  except :
-                     dateBeg  = datetime.datetime(2000,1,1); ie+=1; le.append(reader.line_num)
+                     dateBeg  = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
                  try :
-                     dateEnd  = datetime.datetime.strptime(row[self.numDateBeg],"%Y-%m-%d")
+                     dateEnd  = datetime.datetime.strptime(row[self.numDateEnd],"%Y-%m-%d")
                  except :
-                     dateEnd  = datetime.datetime(2000,1,1); ie+=1; le.append(reader.line_num)
+                     dateEnd  = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
                      
                  if self.debug : print(dateTime,dateBeg,dateEnd)
                  dateTimeV= [dateTime.year,dateTime.month,dateTime.day,dateTime.weekday(),
                              dateTime.hour,dateTime.minute]
                  dateBegV = [dateBeg.year,dateBeg.month,dateBeg.day,dateBeg.weekday()]
                  dateEndV = [dateEnd.year,dateEnd.month,dateEnd.day,dateEnd.weekday()]
-                 datesNN  = [(dateEnd-dateBeg).days+1]
+                 
+                 datesNN  = (dateEnd-dateBeg).days+1
+                 if (not((0<=datesNN) and (datesNN<=200))) : datesNN=0
+                 datesNN=[datesNN]
                  rowX = row[0:self.numDateTime]+dateTimeV+row[(self.numDateTime+1):self.numDateBeg]+dateBegV+dateEndV+datesNN+ \
                             row[(self.numDateEnd+1):self.numBook]+ \
                             [1,1]+ \
@@ -159,16 +295,246 @@ class PreTest (BigCSVFile) :
             print("(0,1,e,list_error)=",i0,i1,ie,le)
             return
         
+def LeakDecision (trainFile,testFile,out1,out2,dirDict=None,
+                  debug=False,debugStop=3000000,
+                  printOK=False) :
+
+    #debug     = True
+    #print     = True
+
+    # for Train file
+
+    numBook     = 18
+    numDateTime = 0
+    numDateBeg  = 11
+    numDateEnd  = 12
+
+    # for leak data
+    numULC      =  5  # user_location_city
+    numODD      =  6  # orig_destination_distance
+    numSDI      = 16  # srch_destination_id
+    numHCluster = 23  # hotel_cluster
+    numHCountry = 21  # hotel_country 
+    numHMarket  = 22  # hotel_market
+    
+    cluster0 = [0]*100
+    
+    d1 = dict();
+    d2 = dict();
+    d3 = dict();
+    d4 = dict();
+    
+    topCluster = cluster0;
+        
+    with open(trainFile, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        i1, i2, i3, i4, ie = 0,0,0,0,0; le =[]
+        for row in reader :
+            if reader.line_num==1: continue
+
+            try :
+                dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
+            except :
+                dateTime = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+                     
+            isBooking = int(row[numBook])
+            ULC       = row[numULC]
+            ODD       = row[numODD]
+            SDI       = row[numSDI]
+            HMarket   = row[numHMarket]
+            HCountry  = row[numHCountry]
+            HCluster  = int(row[numHCluster])
+
+            app1      = 3+17*isBooking
+            app2      = 1+ 5*isBooking
+
+            if ((ULC!="") and (ODD!="")) :
+                 key = (ULC,ODD)
+                 cluster = d1.get(key,[0]*100)
+                 cluster[HCluster] += 1
+                 d1[key]=cluster; i1 +=1;
+
+            if ((SDI!="") and (HCountry!="") and (HMarket!="") and (dateTime.year==2014)) :
+                 key = (SDI,HCountry,HMarket)
+                 cluster = d2.get(key,[0]*100)
+                 cluster[HCluster] += app1
+                 d2[key]=cluster; i2 +=1
+
+            if ((SDI!="")) :
+                 key = (SDI)
+                 cluster = d3.get(key,[0]*100)
+                 cluster[HCluster] += app1
+                 d3[key]=cluster; i3 +=1
+
+            if ((HCountry!="")) :
+                 key = (HCountry)
+                 cluster = d4.get(key,[0]*100)
+                 cluster[HCluster] += app2
+                 d4[key]=cluster; i4 +=1
+
+            topCluster[HCluster] += 1
+
+            if (debug and (reader.line_num>debugStop)) : break
+            if (reader.line_num%100000==0) :
+                print("train :",reader.line_num," d1-4 : ",
+                      sys.getsizeof(d1),"-",len(d1),
+                      sys.getsizeof(d2),"-",len(d2),
+                      sys.getsizeof(d3),"-",len(d3),
+                      sys.getsizeof(d4),"-",len(d4))
+                
+            if (reader.line_num%3000000==0) :
+                if (dirDict!=None) :
+                    print("dumping")
+                    d0 = dict     (line=reader.line_num,
+                                   begin=reader.line_num+1,
+                                   top=topCluster,
+                                   stats=gc.get_stats(),
+                                   count=gc.get_count(),
+                                   thres=gc.get_threshold()
+                                   );
+                    f = open(os.path.join(dirDict,"d0.dict"),"w")
+                    json.dump(d0,f);
+                    f.close();
+                    f = open(os.path.join(dirDict,"d1.dict"),"wb")
+                    pickle.dump(d1,f); f.close();
+                    f = open(os.path.join(dirDict,"d2.dict"),"w")
+                    json.dump(d2,f); f.close();
+                    f = open(os.path.join(dirDict,"d3.dict"),"w")
+                    json.dump(d3,f); f.close();
+                    f = open(os.path.join(dirDict,"d4.dict"),"w")
+                    json.dump(d4,f); f.close();
+                    print("gc",gc.get_count(),gc.get_stats(),gc.get_threshold());
+                    print("end dumping")
+                
+            
+        csvfile.close();
+        
+    print(len(d1),len(d2),len(d3),len(d4))
+    print("dictionary 1",list(d1.keys())[0:1])
+    print(list(d1.values())[0:1])
+    print("dictionary 2",list(d2.keys())[0:1])
+    print(list(d2.values())[0:1])
+    print("dictionary 3",list(d3.keys())[0:1])
+    print(list(d3.values())[0:1])
+    print("dictionary 4",list(d4.keys())[0:1])
+    print(list(d4.values())[0:1])
+    print("topCluster:\n",topCluster)
+
+    print("(0-4)=",i1,i2,i3,i4)
+
+    # for Test file
+
+    numULC      =  6  # user_location_city
+    numODD      =  7  # orig_destination_distance
+    
+    numSDI      = 17  # srch_destination_id
+    numHCountry = 20  # hotel_country 
+    numHMarket  = 21  # hotel_market
+    
+    numDateTime = 1
+    numID       = 0 
+
+    top5x0      = [ str(x[0]) for x in nlargest(5,enumerate(topCluster),key=lambda x:x[1])]
+    print("top5x0",top5x0)
+    
+        
+    with open(testFile, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        i1, i2, i3, i4, ie = 0,0,0,0,0; le =[]
+        out1f  = open(out1,"w")
+        out2f  = open(out2,"w")
+        for row in reader :
+            if reader.line_num==1:
+                print("id,hostel_cluster",file=out1f);
+                print("id,hostel_cluster,mark",file=out2f);
+                continue
+                    
+            try :
+                dateTime = datetime.datetime.strptime(row[self.numDateTime],"%Y-%m-%d %H:%M:%S")
+            except :
+                dateTime = datetime.datetime(2012,1,1); ie+=1; le.append(reader.line_num)
+
+            id        = row[numID]  
+            ULC       = row[numULC]
+            ODD       = row[numODD]
+            SDI       = row[numSDI]
+            HMarket   = row[numHMarket]
+            HCountry  = row[numHCountry]
+
+            out       = []
+            mark      = 0
+            
+            
+            if ((ULC!="") and (ODD!="")) :
+                 key = (ULC,ODD)
+                 if key in d1 :
+                     cluster = d1.get(key)
+                     top5 = nlargest(5,enumerate(cluster),key=lambda x:x[1])
+                     top5 = [str(x[0]) for x in top5]
+                     out  = top5
+                     mark = 1
+                     
+            if ((SDI!="") and (HCountry!="") and (HMarket!="")) :
+                 key = (SDI,HCountry,HMarket)
+                 if key in d2 :
+                     cluster = d2.get(key)
+                     top5 = nlargest(5,enumerate(cluster),key=lambda x:x[1])
+                     top5 = [str(x[0]) for x in top5]
+                     out  = (out + top5)[:5];
+                     mark += 2
+
+            if ((SDI!="")) :
+                 key = (SDI)
+                 if key in d3 :
+                     cluster = d3.get(key)
+                     top5 = nlargest(5,enumerate(cluster),key=lambda x:x[1])
+                     top5 = [str(x[0]) for x in top5]
+                     out  = (out + top5)[:5];
+                     mark += 4
+
+            if ((HCountry!="")) :
+                 key = (HCountry)
+                 if key in d4 :
+                     cluster = d4.get(key)
+                     top5 = nlargest(5,enumerate(cluster),key=lambda x:x[1])
+                     top5 = [str(x[0]) for x in top5]
+                     out  = (out + top5)[:5];
+                     mark += 8
+            if (len(out)<5) : out  = (out+top5x0)[:5]; mark += 16;
+            if (debug and reader.line_num>200) : break;
+            out = (" ".join(out)).strip()
+            print(str(id)+',"'+out+'"',file=out1f);
+            print(str(id)+',"'+out+'",'+str(mark),file=out2f);
+            if (reader.line_num%100000==0) :
+                print("test :",reader.line_num)
+
+
+        csvfile.close()
+        out1f.close()
+        out2f.close()
+            
+
+    return
+        
 
 fTrain = '../Data/train.csv';
 fTest  = '../Data/test.csv';
+fOut1  = '../Result/Leak/df-Leak.csv';
+fOut2  = '../Result/Leak/df-LeakExt.csv';
+fDict  = '../Result/Leak/'
 
 print('-'*50)
 
-strTrain = PreTrain(fTrain);
-print(strTrain.count(),strTrain.getNames())
-#strTrain.readSplitIsBooking();
+if False :
+    strTrain = PreTrain(fTrain);
+    print(strTrain.count(),strTrain.getNames())
+    #strTrain.readSplitIsBooking();
+    strTrain.getLeak();
 
-strTest = PreTest(fTest);
-print(strTest.count(),strTest.getNames())
-strTest.readSplitIsBooking();
+if True :
+    strTest = PreTest(fTest);
+    print(strTest.count(),strTest.getNames())
+    #strTest.readSplitIsBooking();
+
+LeakDecision(fTrain,fTest,fOut1,fOut2,dirDict=fDict,
+             debug=True,debugStop=10000000)
--- R/lookCluster.R
+++ R/lookCluster.R
@@ -1,28 +1,92 @@
 require(fastcluster)
 
+#----------------------------------------------------
 
-cl.i = 9 # user_location_city
+nn.train = names(dtrain.b1)
+nn.test  = names(dtest.b1)
+
+nn = setdiff(intersect(nn.train,nn.test),c("is_booking","cnt","dt0y","dt1y"))
+
+i=15
+
+tt.diff = rep(0,times=length(nn)); names(tt.diff) <- nn
+
+for (i in c(1:length(nn))) {
+
+  tt.train = table(dtrain.b1[nn[i]]); l.train=length(tt.train)
+  tt.test  = table(dtest.b1 [nn[i]]); l.test =length(tt.test)
+  tt.nn    = union(names(tt.train),names(tt.test));
+  
+  tt = rep(0,times=length(tt.nn)); names(tt)=tt.nn;
+  tt[names(tt.train)] = tt.train; tt.train=tt;
+  
+  tt = rep(0,times=length(tt.nn)); names(tt)=tt.nn;
+  tt[names(tt.test)] = tt.test; tt.test=tt;
+  
+  tt.train=tt.train/sum(tt.train)
+  tt.test =tt.test/sum(tt.test)
+  
+  tt.hot  = table(unlist(dtrain.b1[nn[i]]),dtrain.b1$hotel_cluster)
+  tt.hot0 = rowSums(tt.hot)
+
+  tt.diff[i] = sum(abs(tt.train-tt.test));
+  
+  print(sprintf("%i - %s --> %8.6f - %8.6f len=(%i-%i-%i) max=%i",
+                i,nn[i],tt.diff[i],tt.diff[i]/length(tt.nn),
+                l.train,l.test,length(tt.nn),
+                (quantile(tt.hot0))[5]
+                ))
+  #print(quantile(tt.hot0))
+  
+}
+
+plot(tt.diff)
+
+rm(nn,nn.train,nn.test)
+rm(l.train,l.test)
+rm(tt.train,tt.test,tt.nn,tt)
+rm(tt.hot,tt.hot0)
+
+i=31
+tt.hot  = table(unlist(dtrain.b1[nn[i]]),dtrain.b1$hotel_cluster)
+tt.hot0 = rowSums(tt.hot)
+tt.hot.n=tt.hot[,c(1:ncol(tt.hot))]/tt.hot0
+
+tt.clust     = hclust(dist(tt.hot.n),method="ward.D")
+plot(tt.clust)
+tt.memb      = cutree(tt.clust,k=10) # 6-11 clusters
+table(tt.memb)
+plot(table(tt.memb))
+
+centers <- lapply(sort(unique(tt.memb)),
+       function(x){colMeans(tt.hot.n[tt.memb==x,c(1:ncol(tt.hot.n))])})
+
+require(ggplot2)
+
+tt.memb.pca = (tt.memb %in% c(1,3)) #c(1:max(tt.memb)))
+
+pca = prcomp(tt.hot.n[c(1:nrow(tt.hot.n))[tt.memb.pca],])
+tt.hot.pca = predict(pca,tt.hot.n)
 
 
-cl.x = 36 # hotel_cluster
+g1 = ggplot(data = data.frame(pca1=tt.hot.pca[tt.memb.pca,1],
+                              pca2=tt.hot.pca[tt.memb.pca,2],
+                              cl=tt.memb[tt.memb.pca]),
+            aes(x=pca1,y=pca2))+
+     geom_jitter(aes(color=cl),alpha=0.5,fill=0.3,shape=0.2)
+g1
 
-xx = table(dtrain.b1[[35]])
-plot(xx)
-hist(xx)
-quantile(xx); length(xx[is.na(xx)])
+#plot(tt.hot.pca[,1],tt.hot.pca[,2])
 
-ll = table(dtrain.b1$hotel_market,dtrain.b1$hotel_cluster)
-image(ll)
-llx = rowSums(ll)
-hist(llx)
-plot(llx)
-summary(llx)
+x=3; plot(tt.hot.pca[tt.memb==x,1],tt.hot.pca[tt.memb==x,2])
 
-mm = ll
-mm[,c(1:ncol(ll))]=mm[,c(1:ncol(ll))]/llx
-
-contour(c(1:nrow(mm)),c(1:ncol(mm)),mm,levels = seq(0.01,1.0,by=0.1))
+(sort(abs(pca$rotation[,1]),decreasing = TRUE))[1:10]
 
 
+stop("Normal end")
+
+#----------------------------------------------------
+#image(ll)
+#contour(c(1:nrow(mm)),c(1:ncol(mm)),mm,levels = seq(0.01,1.0,by=0.1))
 #------------------- function
 
